9072eb8df3ac13dc0fd34cb64a852e210d0eab72
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-03-07 22:47:47 +0600

    Add files via upload

---
+++ WaterMekon/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WaterMekon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w;
+            w = Int32.Parse(Console.ReadLine());
+            if (w > 2 && w % 2 == 0)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");                
+        }
+    }
+}
