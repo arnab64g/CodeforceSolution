11314df34898c99c3c0675e79167d1b07d590a19
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-03-09 22:25:57 +0600

    Add files via upload

---
+++ Word.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int l = s.length(), count = 0;
+    while (l--)
+    {
+        if(isupper(s[l]))
+            count++;
+    }
+    if (count * 2 > s.length()){
+        for (int i = 0; i < s.length(); i++)
+            cout << (char)toupper(s[i]);
+    }
+    else{
+        for (int i = 0; i < s.length(); i++)
+            cout << (char)tolower(s[i]);
+    }
+    return 0;
+}
