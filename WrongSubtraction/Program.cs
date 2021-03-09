e08228b1cc99c0aa67c683f72cf22467f9fbe478
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-03-09 20:25:12 +0600

    Add files via upload

---
+++ WrongSubtraction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WrongSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            string []s = Console.ReadLine().Split();
+            n = int.Parse(s[0]);
+            k = int.Parse(s[1]);
+            while (k != 0)
+            {
+                if(n%10 == 0)
+                    n /= 10;
+                else n--;
+                k--;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
