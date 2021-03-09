4c9059bab7dbd01834e1f3a8f00197046b4f0f04
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-03-09 20:25:57 +0600

    Add files via upload

---
+++ BearAndBigBrother/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BearAndBigBrother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l, b, count = 0;
+            string []s = Console.ReadLine().Split(' ');
+            l = int.Parse(s[0]);
+            b = int.Parse(s[1]);
+            while (true)
+            {
+                l *= 3;
+                b *= 2;
+                count++;
+                if (l > b)
+                    break;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
