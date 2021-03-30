7f69a0148a9621a943b284eb6488686e66a6dccf
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-03-30 15:26:40 +0600

    Add files via upload

---
+++ VasyaTheHipster.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class VasyaTheHipster {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r, b, mxd, mxs;
+        r = sc.nextInt();
+        b = sc.nextInt();
+        mxd = (r < b) ? r : b;
+        mxs = (r > b) ? r : b;
+        mxs -= mxd;
+        mxs /= 2;
+        sc.close();
+        System.out.println(mxd+" "+mxs);
+    }
+}
