da511f3510d1651aacaddfdc58fc9133af7f32e1
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-02 20:31:38 +0600

    Add files via upload

---
+++ MinutesBeforeTheNewYear.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int t, h, m, tm;
+    scanf("%d", &t);
+    while (t--){
+        scanf("%d %d", &h, &m);
+        tm = 0;
+        tm += (23-h)*60;
+        tm += 60 - m;
+        printf("%d\n", tm);
+    }
+    return 0;
+}
