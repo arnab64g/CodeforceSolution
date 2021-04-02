90ff1750e6cff6fca7dd26f8fcc6860416954e33
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-02 15:21:17 +0600

    Add files via upload

---
+++ CardsForFriends.py
@@ -0,0 +1,18 @@
+n = int(input())
+for i in range(n):
+    h, w, n = input().split()
+    h = int(h)
+    w = int(w)
+    n = int(n)
+    a, b = 1, 1
+    while w%2 == 0:
+        a *= 2
+        w /=2
+    while h%2 == 0:
+        b *= 2
+        h /=2
+    total= a*b
+    if total >= n:
+        print("YES")
+    else:
+        print ("NO")
