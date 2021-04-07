9d420051a16cce43bcde1876b9c81ef1e52fb69d
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-07 17:24:12 +0600

    Add files via upload

---
+++ BrainsPhotos.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BrainsPhotos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int pix = m*n;
+        char ch;
+        boolean res = true;
+        for (int i = 0; i < pix; i++) {
+            ch = sc.next().charAt(0);
+            if(ch == 'C' || ch == 'M' || ch == 'Y')
+                res = false;
+        }
+        if(!res)
+            System.out.println("#Color");
+        else System.out.println("#Black&White");
+    }
+}
