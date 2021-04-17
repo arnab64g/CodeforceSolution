4fcbd6277fa22ee496ee1715cea11d1238c3fe77
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-17 22:53:53 +0600

    Update Program.cs

--- WrongSubtraction/Program.cs
+++ WrongSubtraction/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace WrongSubtraction
 {
     class Program
@@ -10,8 +9,7 @@ namespace WrongSubtraction
             string []s = Console.ReadLine().Split();
             n = int.Parse(s[0]);
             k = int.Parse(s[1]);
-            while (k != 0)
-            {
+            while (k != 0){
                 if(n%10 == 0)
                     n /= 10;
                 else n--;
