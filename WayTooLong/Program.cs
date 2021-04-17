514d401a5722aa350d012472806bbc6589910bdd
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-17 22:53:07 +0600

    Update Program.cs

--- WayTooLong/Program.cs
+++ WayTooLong/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace WordCount
 {
     class Program
@@ -9,15 +8,11 @@ namespace WordCount
             int n;
             n = int.Parse(Console.ReadLine());
             String s, ns;
-            for (int i = 0; i < n; i++)
-            {
+            for (int i = 0; i < n; i++){
                 s = Console.ReadLine();
                 if (s.Length <= 10)
-                {
                     Console.WriteLine(s);                    
-                }
-                else
-                {
+                else{
                     ns = "";
                     ns += s[0];
                     ns += Convert.ToString(s.Length - 2);
