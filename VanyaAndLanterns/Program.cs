3e1e180fd8965c7adbfa487f71334a1f1f6334f6
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-17 22:55:19 +0600

    Update Program.cs

--- VanyaAndLanterns/Program.cs
+++ VanyaAndLanterns/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace VanyaAndLanterns
 {
     class Program
@@ -14,19 +13,15 @@ namespace VanyaAndLanterns
             s = Console.ReadLine().Split(' ');
             int[] lntr = new int[n];
             for (int i = 0; i < n; i++)
-                lntr[i] = int.Parse(s[i]);
-               
+                lntr[i] = int.Parse(s[i]); 
             Array.Sort(lntr);
             int max =  (2*lntr[0] > 2*(l - lntr[n-1])) ? 2*lntr[0] : 2*(l - lntr[n-1]);
-            for (int i = 1; i < n; i++)
-            {
+            for (int i = 1; i < n; i++){
                 distance = lntr[i]- lntr[i-1];
                 if(distance > max)
                     max = distance;
             }
-   
             mind = (double)max/2;
-           
             Console.WriteLine(string.Format("{0:0.0000000000}", mind));
         }
     }
