7cb75239a48afabde7c20422903bb6923ec4141b
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-17 23:27:41 +0600

    Update GiftsFixing.java

--- GiftsFixing.java
+++ GiftsFixing.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
-
 /**
  * GiftsFixing
  */
 public class GiftsFixing {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         int n;
         long mina, minb, count, difa, difb;
         long[] a, b;
-        
         for (int i = 0; i < t; i++) {
             n = sc.nextInt();
             a = new long[n];
