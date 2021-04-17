60228ca08fb32050b32a9df3d708771d375c936f
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-17 22:55:59 +0600

    Update Program.cs

--- HelpfulMath/Program.cs
+++ HelpfulMath/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace HelpfulMath
 {
     class Program
@@ -9,9 +8,8 @@ namespace HelpfulMath
             string []s = Console.ReadLine().Split('+');
             string ns = "";
             int []a = new int[s.Length];
-            for(int i = 0; i < s.Length; i++){
+            for(int i = 0; i < s.Length; i++)
                 a[i] = int.Parse(s[i]);                
-            }
             Array.Sort(a, 0, s.Length);
             ns += a[0];
             for (int i = 1; i < s.Length; i++){
