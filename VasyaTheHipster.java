994c5015b7068240f6c3b304ffba59b2e88d414f
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-17 23:18:20 +0600

    Update VasyaTheHipster.java

--- VasyaTheHipster.java
+++ VasyaTheHipster.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class VasyaTheHipster {
+public class VasyaTheHipster{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int r, b, mxd, mxs;
