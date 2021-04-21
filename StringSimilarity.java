0b673674b4fdd95b0d19591f98a1bd906912961c
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-04-21 23:29:58 +0600

    Add files via upload

---
+++ StringSimilarity.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StringSimilarity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int l = 2*n-1;
+            String s = sc.next();
+            char[] ch = s.toCharArray();
+            String empty = "";
+            empty += ch[0];           
+            for (int j = 1; j < n; j++) 
+                empty += ch[j*2];
+            System.out.println(empty);
+        }
+        sc.close();
+    }
+}
