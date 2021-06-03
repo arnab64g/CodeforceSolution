d776cc976d97852ba2209cb8bd4f024c50336ebd
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-06-03 22:59:29 +0600

    Add files via upload

---
+++ MoveBrackets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MoveBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++){
+                int l = int.Parse(Console.ReadLine());
+                string s = Console.ReadLine();
+                int openning = 0, closeng = 0;
+                for (int j = 0; j < l; j++){
+                    if (s[j] == '(')
+                        openning++;
+                    else{
+                        if(openning == 0)
+                            closeng++;
+                        else openning--;
+                    }
+                }
+                Console.WriteLine(closeng);
+            }
+        }
+    }
+}
