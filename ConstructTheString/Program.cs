f506976c5efe7020f40e19131a70cf4f4d03cee5
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-06-13 20:29:46 +0600

    Add files via upload

---
+++ ConstructTheString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ConstructTheString
+{
+    class Program
+    {
+        static string StringGen(int n, int a, int b){
+            string str = "";
+            for (int i = 0; i < b; i++)
+                str += Convert.ToChar(97+i);
+            for (int i = b ; i < a; i++)
+                str += str[i - b];
+            for (int i = a; i < n; i++)
+                str += str[i - a];
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++){
+                string[] s = Console.ReadLine().Split(' ');
+                int n = int.Parse(s[0]);
+                int a = int.Parse(s[1]);
+                int b = int.Parse(s[2]);
+                Console.WriteLine(StringGen(n, a, b));
+            }
+        }
+    }
+}
