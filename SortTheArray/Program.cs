fd80f7edb8b831368545002caf7c1fe40d226b20
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-06-16 20:31:30 +0600

    Add files via upload

---
+++ SortTheArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SortTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split(' ');
+            int[] a = new int[n];
+            int[] b = new int[n];
+            for (int i = 0; i < n; i++){
+                a[i] = int.Parse(s[i]);
+                b[i] = a[i];
+            }
+            Array.Sort(b);
+            int v1 = 0, v2 = 0;;
+            for (int i = 0; i < n; i++)
+                if(a[i] != b[i]){
+                    v1 = i;
+                    break;
+                }
+            for (int i = n - 1; i >= 0; i--)
+                if (a[i] != b[i]){
+                    v2 = i;
+                    break;
+                }
+            bool r = true;
+            for (int i = v1, j = v2; i <= v2 || j >= v1; i++, j--){
+              
+                if (a[i] != b[j]){
+                    r = false;
+                    break;
+                }
+            }
+            if(r){
+                Console.WriteLine("yes");
+                Console.WriteLine(++v1 + " " + ++v2);
+            }
+            else Console.WriteLine("no");
+        }
+    }
+}
