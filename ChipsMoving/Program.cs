cd4e66468e0255ef2d6b13ff3e69aa41fb6ee536
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-07-07 23:41:05 +0600

    Add files via upload

---
+++ ChipsMoving/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChipsMoving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split(' ');
+            int cor;
+            int odds = 0, evens = 0;
+            for (int i = 0; i < n; i++){
+                cor = int.Parse(s[i]);
+                if(cor % 2 == 0)
+                    evens++;
+                else odds++;
+            }
+            if(evens < odds)
+                Console.WriteLine(evens);
+            else Console.WriteLine(odds);
+        }
+    }
+}
