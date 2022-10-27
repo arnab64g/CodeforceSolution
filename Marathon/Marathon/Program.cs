c69e516b3acc967607912dc7004135537eb03f42
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2022-10-27 22:01:43 +0600

    Add files via upload

---
+++ Marathon/Marathon/Program.cs
@@ -0,0 +1,15 @@
+﻿int t = int.Parse(Console.ReadLine());
+for (int i = 0; i < t; i++)
+{
+    string val = Console.ReadLine();
+    string[] m = val.Split(' ');
+    int count = 0;
+    for (int j = 1; j < 4; j++)
+    {
+        if (int.Parse(m[0]) < int.Parse(m[j]))
+        {
+            count++;
+        }
+                }
+    Console.WriteLine(count);
+}
