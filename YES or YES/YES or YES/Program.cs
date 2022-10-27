e6f28fda9733024307cb2c0e88c3a18977393301
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2022-10-27 22:14:30 +0600

    Add files via upload

---
+++ YES or YES/YES or YES/Program.cs
@@ -0,0 +1,14 @@
+﻿int t = int.Parse(Console.ReadLine());
+for (int i = 0; i < t; i++)
+{
+    string s = Console.ReadLine();
+    s = s.ToUpper();
+    if(s == "YES")
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
