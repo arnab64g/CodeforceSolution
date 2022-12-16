8aa5e82a258de848fe3422689a3282365564afc6
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2022-12-16 20:12:25 +0600

    Add files via upload

---
+++ Equal Candies/Equal Candies/Program.cs
@@ -0,0 +1,27 @@
+﻿int t = int.Parse(Console.ReadLine());
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    string[] s = Console.ReadLine().Split(' ');
+    int[] ints = new int[s.Length];
+    
+    for (int j = 0; j < s.Length; j++)
+    {
+        ints[j] = int.Parse(s[j]);
+    }
+
+    Array.Sort(ints);
+    if (ints[0] == ints[s.Length-1])
+    {
+        Console.WriteLine("0");
+    }
+    else 
+    {
+        long count = 0;
+        for (int j = 1; j < s.Length; j++)
+        {
+            count+= ints[j] - ints[0];
+        }
+        Console.WriteLine(count);
+    }
+}
