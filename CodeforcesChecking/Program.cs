3e2a607fc8c7f7c39e317971c92fe5e74aaf248a
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2023-02-27 09:33:04 +0600

    453

--- CodeforcesChecking/Program.cs
+++ CodeforcesChecking/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿int n = int.Parse(Console.ReadLine());
+
+List<string> ch = new List<string>();
+
+ch.Add("c");
+ch.Add("o");
+ch.Add("d");
+ch.Add("e");
+ch.Add("f");
+ch.Add("r");
+ch.Add("s");
+
+for (int i = 0; i < n; i++)
+{
+    string str = Console.ReadLine();
+
+    if (ch.Contains(str))
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
