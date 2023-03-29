c5634973c7aa0af86c90e1e87d6399d659d66473
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2023-03-29 10:23:22 +0600

    1760B

---
+++ AtillasFavoriteProblem/AtillasFavoriteProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿int t = int.Parse(Console.ReadLine());
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    char[] str = Console.ReadLine().ToCharArray();
+    int max = 0, val;
+
+    for (int j = 0; j < n; j++)
+    {
+        val = str[j] - 'a' + 1;
+        if (val > max) max = val;
+    }
+
+    Console.WriteLine(max);
+}
