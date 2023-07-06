19db60dd42d14434b7636ae003e2ceb325e9a73d
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2023-07-06 14:26:18 +0600

    Add files via upload

---
+++ Blank Space/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+int t = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    string[] str = Console.ReadLine().Split(' ');
+
+    int longets = 0, val = 0;
+    for (int j = 0; j < n; j++)
+    {
+        if (str[j] == "0")
+        {
+            val++;
+            if (val > longets)
+            {
+                longets = val;
+            }
+        }
+        else
+        {
+            val = 0;
+        }
+    }
+    Console.WriteLine(longets);
+}
