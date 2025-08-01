b767e9d92bf558bbf06ccee1ef9383b32fd5dfc8
arnab64g <arnab64g@gmail.com>
2025-08-01 21:26:32 +0600

    S

---
+++ Python/Minimize.py
@@ -0,0 +1,5 @@
+n = int(input())
+
+for i in range(n):
+    values = input().split(' ')
+    print(int(values[1]) - int(values[0]))
---
+++ Python/ShortSort.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(n):
+    values = input()
+    a = list(values)
+    if a[0] == 'a' or a[1] == 'b' or a[2] == 'c':
+        print('Yes')
+    else:
+        print('No')
