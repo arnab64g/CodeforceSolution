0fc4577245ce83d1311682b92ceaa6a72ff04eea
arnab64g <arnab64g@gmail.com>
2025-08-17 09:59:44 +0600

    S

---
+++ C/WordOnThePaper.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    scanf("%d", &n);
+    while (n--)
+    {
+        char word[65];
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            char str[9];
+            scanf("%s", str);
+            for (int j = 0; j < 8; j++)
+            {
+                if(str[j] != '.'){
+                    word[count] = str[j];
+                    count++;
+                }
+            }
+        }
+        word[count] = '\0';
+        printf("%s\n", word);
+    }
+    
+    return 0;
+}
