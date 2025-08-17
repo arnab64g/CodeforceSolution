4d01dfcf3dff70aaac4d8cde01b6b2b22f786465
arnab64g <arnab64g@gmail.com>
2025-08-17 21:21:46 +0600

    495

---
+++ Python/YogurtSale.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+for i in range(t):
+    str = input().split(' ')
+
+    n = int(str[0])
+    a = int(str[1])
+    b = int(str[2])
+    
+    total = 0
+
+    if a * 2 <= b:
+        total = a * n
+    else:
+        total = b * (n//2)
+        if n % 2 == 1:
+            total += a
+
+    print(total)
