02539cc8daa554b8c9809bc5b3f6426ef834b494
arnab64g <arnab64g@gmail.com>
2025-08-18 10:17:44 +0600

    500

---
+++ Python/OnlyPluses.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+for i in range(t):
+    str = input().split(' ')
+    a = int(str[0])
+    b = int(str[1])
+    c = int(str[2])
+
+    for j in range(5):
+        if a <= b and a <= c:
+            a += 1
+        elif b <= a and b <= c:
+            b += 1
+        elif c <= a and c <= b:
+            c += 1
+    
+    print(a * b * c)    
